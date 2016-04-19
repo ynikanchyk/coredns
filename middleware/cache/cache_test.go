8ea7aaec447b1208ed4c1476b86e464ef80d3883
Miek Gieben <miek@miek.nl>
2016-04-19 07:36:00 +0100

    cache test infrastructure

--- middleware/cache/cache_test.go
+++ middleware/cache/cache_test.go
@@ -10,18 +10,37 @@ import (
 	"github.com/miekg/dns"
 )
 
-func msgTestMiekMx() *dns.Msg {
-	m := new(dns.Msg)
-	m.SetQuestion("miek.nl.", dns.TypeMX)
-	m.RecursionAvailable = true
-	m.AuthenticatedData = true
-	m.Answer = []dns.RR{
-		test.MX("miek.nl.	1800	IN	MX	1 aspmx.l.google.com."),
-		test.MX("miek.nl.	1800	IN	MX	5 alt1.aspmx.l.google.com."),
-		test.MX("miek.nl.	1800	IN	MX	5 alt2.aspmx.l.google.com."),
-		test.MX("miek.nl.	1800	IN	MX	10 aspmx2.googlemail.com."),
-		test.MX("miek.nl.	1800	IN	MX	10 aspmx3.googlemail.com."),
-	}
+type cacheTestCase struct {
+	test.Case
+	AuthenticatedData  bool
+	Authoritative      bool
+	RecursionAvailable bool
+	Truncated          bool
+}
+
+var cacheTestCases = []cacheTestCase{
+	{
+		RecursionAvailable: true, AuthenticatedData: true, Authoritative: true,
+		Case: test.Case{
+			Qname: "miek.nl.", Qtype: dns.TypeMX,
+			Answer: []dns.RR{
+				test.MX("miek.nl.	1800	IN	MX	1 aspmx.l.google.com."),
+				test.MX("miek.nl.	1800	IN	MX	10 aspmx2.googlemail.com."),
+				test.MX("miek.nl.	1800	IN	MX	10 aspmx3.googlemail.com."),
+				test.MX("miek.nl.	1800	IN	MX	5 alt1.aspmx.l.google.com."),
+				test.MX("miek.nl.	1800	IN	MX	5 alt2.aspmx.l.google.com."),
+			},
+		},
+	},
+}
+
+func cacheMsg(m *dns.Msg, tc cacheTestCase) *dns.Msg {
+	m.RecursionAvailable = tc.RecursionAvailable
+	m.AuthenticatedData = tc.AuthenticatedData
+	m.Authoritative = tc.Authoritative
+	m.Truncated = tc.Truncated
+	m.Answer = tc.Answer
+	m.Ns = tc.Ns
 	return m
 }
 
@@ -33,44 +52,22 @@ func newTestCache() (Cache, *CachingResponseWriter) {
 
 func TestCacheSetGet(t *testing.T) {
 	c, crr := newTestCache()
-	res := msgTestMiekMx()
-	mt, opt := classify(res)
-	do := false
-	if opt != nil {
-		do = opt.Do()
+
+	for _, tc := range cacheTestCases {
+		m := tc.Msg()
+		m = cacheMsg(m, tc)
+
+		mt, _ := classify(m)
+		do := tc.Case.Do
+		key := cacheKey(m, mt, do)
+		crr.Set(m, key, mt)
+
+		name := middleware.Name(m.Question[0].Name).Normalize()
+		qtype := m.Question[0].Qtype
+
+		if i, ok := c.Get(name, qtype, do); ok {
+			resp := i.toMsg(m)
+			t.Logf("%s\n", resp.String())
+		}
 	}
-
-	key := cacheKey(res, mt, do)
-	crr.Set(res, key, mt)
-
-	// TODO(miek): make this somewhat better and loop through a few messages.
-	name := middleware.Name(res.Question[0].Name).Normalize()
-	qtype := res.Question[0].Qtype
-
-	if i, ok := c.Get(name, qtype, do); ok {
-		resp := i.toMsg(res)
-		t.Logf("%s\n", resp.String())
-	}
-
-	time.Sleep(2 * time.Second)
-
-	if i, ok := c.Get(name, qtype, do); ok {
-		resp := i.toMsg(res)
-		t.Logf("%s\n", resp.String())
-	}
-}
-
-func TestCacheTruncated(t *testing.T) {
-	res := msgTestMiekMx()
-	res.Truncated = true
-	mt, _ := classify(res)
-	key := cacheKey(res, mt, true)
-	if key != "" {
-		t.Errorf("Truncated message should lead to empty cache key, got %s", key)
-	}
-}
-
-// TODO(miek)
-func TestClassify(t *testing.T) {
-
 }
