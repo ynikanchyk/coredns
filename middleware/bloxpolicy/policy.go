b069899f653b6fec95a1296f05bb4b5c36fe174a
Michael Richmond <mrichmond@infoblox.com>
2016-06-13 17:30:57 -0700

    Adding basic blocking policy based on existence of file "block"

--- middleware/bloxpolicy/policy.go
+++ middleware/bloxpolicy/policy.go
@@ -1,18 +1,43 @@
 package bloxpolicy
 
+import (
+	"fmt"
+	"os"
+)
+
+// blockFilePolicy returns true if the file "block" exists in the pwd beside coredns
+// Note: this function exists to simulate calling an external policy engine
+func blockFilePolicy() (bool, error) {
+
+	allow := false
+
+	_, err := os.Stat("./block")
+	if err != nil {
+		if os.IsNotExist(err) {
+			// file "./block" does not exist, therefore client is allowed, and
+			// err has been handled.
+			allow = true
+			err = nil
+		} else {
+			// some other error occured, therefore default to disallow
+			allow = false
+		}
+	} else {
+		allow = false
+		err = nil
+	}
+	
+	return allow, err
+}
+
 
 // IsClientAllowed returns true if DNS lookups are permitted from the client
 // based on the blox policy engine.
 func (p BloxPolicy) IsClientAllowed(client string) (bool, error) {
 
-	// Initially hard-corded
-	allowed := true
+	allow, err := blockFilePolicy()
+	// TODO: JB integration here instead of call to blockFilePolicy
 
-	var err error 
-
-	
-	// TODO: JB integration here
-
-
-	return allowed, err
+	fmt.Printf("#### exit IsClientAllowed: %v,%v\n", allow, err)
+	return allow, err
 }
